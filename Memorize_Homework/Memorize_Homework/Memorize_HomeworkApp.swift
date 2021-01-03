b09c7ffd52e121885f26b17fc6a8538c67dd4dca
sckimynwa <tsb03672@naver.com>
2021-01-03 16:39:54 +0900

    [Lecture4] remove existing homework & create new

---
+++ Memorize_Homework/Memorize_Homework/Memorize_HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_HomeworkApp.swift
+//  Memorize_Homework
+//
+//  Created by Kim Sang Chul on 2021/01/03.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
